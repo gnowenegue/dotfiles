40121869ca075576de182d14a037fbbee4eb39b6
Eugene Wong <gnow.enegue@gmail.com>
2021-08-30 19:43:32 +0800

    Refactor

--- install-brew-aliases.sh
+++ install-brew-aliases.sh
@@ -1,5 +1,11 @@
 #!/bin/zsh
 
-BIN_DIR='/usr/local/bin'
+BIN_DIR="/usr/local/bin"
+BREW_ALIASES_DIR="$HOME/.brew-aliases"
+MY_BREW_ALIASES_DIR="$HOME/dotfiles/brew-aliases"
 
-for FILE in ~/dotfiles/brew-aliases/*; do ln -vs $FILE $BIN_DIR/brew-$(basename $FILE); done
+ln -vs $MY_BREW_ALIASES_DIR/* $BREW_ALIASES_DIR
+
+for FILE in $MY_BREW_ALIASES_DIR/*;
+  do ln -vs $FILE $BIN_DIR/brew-$(basename $FILE);
+done
